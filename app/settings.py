6a7c6232c8ed1a2a31b43ec4706d48890bd7fc90
Lucas Bessa <lucas.bessafp72@gmail.com>
2023-01-10 11:34:09 -0400

    primeira model e migrations

--- app/settings.py
+++ app/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ["0.0.0.0", "127.0.0.1", "localhost"]
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -103,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Boa_Vista'
 
 USE_I18N = True
 
