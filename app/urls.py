b39e009e785a6a08e5d088ff1bc71d4dc5a2e8ff
Lucas Bessa <lucas.bessafp72@gmail.com>
2023-01-16 11:33:40 -0400

    pwadmin inicio

--- app/urls.py
+++ app/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path("", include("pwadmin.urls")),
     path("polls/", include("polls.urls")),
     path('admin/', admin.site.urls),
     path("__reload__/", include("django_browser_reload.urls"))
