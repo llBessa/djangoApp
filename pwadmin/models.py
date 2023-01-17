a9d9e61c53752484087763345657e7f24e4c5026
Lucas Bessa <lucas.bessa@ufrr.br>
2023-01-17 11:28:38 -0400

    pwadmin models e primeiras views

--- pwadmin/models.py
+++ pwadmin/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Users(models.Model):
+    name = models.CharField(max_length=200, null=False)
+    email = models.CharField(max_length=320, null=False)
+    password = models.CharField(max_length=30, null=False)
+    created_at = models.DateTimeField(null=False)
+
+class Passwords(models.Model):
+    user = models.ForeignKey(Users, on_delete=models.CASCADE)
+    label = models.CharField(max_length=50, null=False)
+    password = models.CharField(max_length=100, null=False)
